97d7ac2454474bc67e0ecfa105affc916873eb95
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-03-12 16:15:51 +0530

    bye

--- lib/view/menu/items/contacts/add_contact.dart
+++ lib/view/menu/items/contacts/add_contact.dart
@@ -21,7 +21,7 @@ class AddContact extends StatelessWidget {
       body:  Column(
         children: [
           Container(
-            margin:  EdgeInsets.symmetric(horizontal: 5.w),
+            margin:  EdgeInsets.symmetric(horizontal: 10.w),
             child: Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
@@ -30,7 +30,7 @@ class AddContact extends StatelessWidget {
                Row(
                  children: [
                    Container(
-                     margin: EdgeInsets.only(right: 80.w,top: 8.w,),
+                     margin: EdgeInsets.only(right: 5.w,top: 8.w,),
                      child: Stack(
                        children: [
                          Positioned(
@@ -48,24 +48,36 @@ class AddContact extends StatelessWidget {
                                    )),
                              ),
                            ),
-                         )
+                         ),
+
                        ],
                      ),
                    ),
-                   Container(
-                     margin: EdgeInsets.only(right: 12.w),
-                     decoration: BoxDecoration(
-                       color: Color(0xffB4C6D4),
-                       shape: BoxShape.circle,
-                     ),
-                     width: 24.w,
-                     height: 24.h,
-                     child: Center(
-                       child:Icon(
-                         Icons.cancel_outlined,
-                         color: Colors.white,
+
+                   Row(
+                     children: [
+                       Container(
+                         margin: EdgeInsets.only(right: 40.w),
+
+                         child: Image.asset("assets/contacts/ami.png"),
                        ),
-                     ),
+                       Container(
+                         margin: EdgeInsets.only(right: 12.w),
+                         decoration: BoxDecoration(
+                           color: Color(0xffB4C6D4),
+                           shape: BoxShape.circle,
+                         ),
+                         width: 24.w,
+                         height: 24.h,
+                         child: Center(
+                           child:Icon(
+                             Icons.cancel_outlined,
+                             color: Colors.white,
+                           ),
+                         ),
+                       ),
+
+                     ],
                    ),
                  ],
                )
@@ -73,8 +85,50 @@ class AddContact extends StatelessWidget {
             ),
           ),
 
+          BusinessCard()
+
         ],
       )
+    );
+  }
+}
+
+
+class BusinessCard extends StatelessWidget {
+  const BusinessCard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20.h),
+      width: 336.w,
+      height: 216.h,
+      decoration: BoxDecoration(
+        color: const Color(0xffF5F6F9),
+        borderRadius: BorderRadius.circular(8.sp),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+
+            Container(
+              child: Center(
+                child : Icon(Icons.add,color: Colors.white,),
+              ),
+              width: 22.w,
+              height: 22.h,
+              decoration: BoxDecoration(
+                color: AppColors.primaryColor,
+                shape: BoxShape.circle,
+              ),
+            ),
+            Container(child: Image.asset("assets/contacts/add.png"),),
+            Container(
+              child: Text("add business card" , style: TextStyle(fontWeight: FontWeight.w500 , fontFamily: "roboto_medium" , fontSize: 18.sp),),)
+          ],
+        ),
+      ),
     );
   }
 }
