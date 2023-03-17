8d98262217520a5f99a06ed06184121593844f90
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-03-17 18:19:09 +0530

    break

--- lib/view/menu/items/contacts/Details.dart
+++ lib/view/menu/items/contacts/Details.dart
@@ -121,3 +121,402 @@ class ContactDetails extends StatelessWidget {
         );
   }
 }
+
+class DetailRow extends StatefulWidget {
+  @override
+  _MyRowState createState() => _MyRowState();
+}
+
+class _MyRowState extends State<DetailRow> {
+  bool _isOpen = false;
+
+  void _toggleDropdown() {
+    setState(() {
+      _isOpen = !_isOpen;
+    });
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+        children:[
+          Container(
+            margin: EdgeInsets.only(top: 15.h  , left: 16.w , right: 16.w),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Contact Details' ,style: TextStyle(color: AppColors.primaryColor,fontFamily: "roboto_medium" ,
+                        fontSize: 14.sp),),
+
+
+                  ],
+                ),
+                GestureDetector(
+                    onTap: _toggleDropdown,
+                    child: Icon(_isOpen ? Icons.arrow_drop_up : Icons.arrow_drop_down)),
+              ],
+            ),
+          ),
+
+
+          if(_isOpen)
+            SingleChildScrollView(
+              child: Column(
+
+                children: [
+
+                  Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      children:[
+
+
+                        Detaling(title: 'contact name', subTitle: 'Mr. Radha R Krishna',),
+                        SecondTypeDetailing(title: 'Account name', subTitle: 'Mr. Radha R Krishna',),
+                        // ThirdTypeDetailing(),
+                        ThirdTypeDetailing(
+
+                          title1: 'Department',
+                          subTitle1: 'Sales',
+                          title2: 'Designation',
+                          subTitle2: 'Sales Manager',
+                        ),
+
+                        FourthTypeDetailing(
+
+                          title1: 'Mobile number',
+                          subTitle1: '99999 12345',
+                          title2: 'Mobile number',
+                          subTitle2: '99999 12346',
+                        ), FourthTypeDetailing(
+
+                          title1: 'Work Phone',
+                          subTitle1: '08022225555',
+                          title2: 'Residence Phone',
+                          subTitle2: '08033335555',
+                        ),
+
+
+                        // ThirdTypeDetailing(
+                        //
+                        //   title1: 'Mobile number',
+                        //   subTitle1: '99999 12345',
+                        //   title2: 'alt. Mobile number',
+                        //   subTitle2: '99999 12346',
+                        //
+                        // ),
+                      ]
+                  ),
+
+
+
+
+                ],
+              ),
+            )
+        ]
+    );
+  }
+
+}
+
+
+class Detaling extends StatelessWidget {
+  const Detaling({Key? key, required this.title, required this.subTitle}) : super(key: key);
+
+  final String title;
+  final String subTitle;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 16.h , top: 10.h,right: 16.w),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(title ,style:TextStyle(color: Color(0xff00A6D6) ,fontFamily: "roboto_bold" , fontSize: 13.sp)  ),
+                    ],
+                  )
+
+                  ,Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(subTitle, style: TextStyle(
+
+                        fontSize: 16.sp,
+                        fontFamily: "roboto_bold",
+                        color: AppColors.primaryColor,
+                        fontWeight: FontWeight.w400,
+                      ),),
+                      Image.asset("assets/contacts/web.png")
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        )
+      ],
+    );
+  }
+}
+
+class SecondTypeDetailing extends StatelessWidget {
+  const SecondTypeDetailing(
+      {Key? key, required this.title, required this.subTitle})
+      : super(key: key);
+  final String title;
+  final String subTitle;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 10.h,),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding: EdgeInsets.only(left: 16.h, top: 10.h, right: 16.w),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(title, style: TextStyle(color: Color(0xff00A6D6),
+                            fontFamily: "roboto_bold",
+                            fontSize: 13.sp)),
+                      ],
+                    )
+
+                    , Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(subTitle, style: TextStyle(
+
+                          fontSize: 16.sp,
+                          fontFamily: "roboto_bold",
+                          color: AppColors.primaryColor,
+                          fontWeight: FontWeight.w400,
+                        ),),
+                      ],
+                    )
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+class ThirdTypeDetailing extends StatelessWidget {
+   ThirdTypeDetailing({Key? key, required this.title1,
+     required this.subTitle1, required this.title2, required this.subTitle2}) : super(key: key);
+
+  final String title1;
+  final String subTitle1;
+  final String title2;
+  final String subTitle2;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 16.h, top: 20.h, right: 16.w),
+      child: Column(
+        children: [
+
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(title1, style: TextStyle(color: Color(0xff00A6D6),
+                          fontFamily: "roboto_bold",
+                          fontSize: 13.sp)),
+                    ],
+                  )
+
+                  , Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(subTitle1, style: TextStyle(
+
+                        fontSize: 16.sp,
+                        fontFamily: "roboto_bold",
+                        color: AppColors.primaryColor,
+                        fontWeight: FontWeight.w400,
+                      ),),
+                      Container(
+                        margin: EdgeInsets.only(left: 10.w),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 125.w),
+                    child: Row(
+
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(title2, style: TextStyle(color: Color(0xff00A6D6),
+                            fontFamily: "roboto_bold",
+                            fontSize: 13.sp)),
+                      ],
+                    ),
+                  )
+
+                  , Container(
+                    margin: EdgeInsets.only(left: 125.w),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(subTitle2, style: TextStyle(
+
+                              fontSize: 16.sp,
+                              fontFamily: "roboto_bold",
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w400,
+                            ),),
+                            Container(
+                              margin: EdgeInsets.only(left: 10.w),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class FourthTypeDetailing extends StatelessWidget {
+  const FourthTypeDetailing({Key? key, required this.title1, required this.subTitle1, required this.title2, required this.subTitle2}) : super(key: key);
+
+  final String title1;
+  final String subTitle1;
+  final String title2;
+  final String subTitle2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.only(left: 16.h, top: 20.h, right: 16.w),
+      child: Column(
+        children: [
+
+          Row(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(title1, style: TextStyle(color: Color(0xff00A6D6),
+                          fontFamily: "roboto_bold",
+                          fontSize: 13.sp)),
+                    ],
+                  )
+
+                  , Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(subTitle1, style: TextStyle(
+
+                        fontSize: 16.sp,
+                        fontFamily: "roboto_bold",
+                        color: AppColors.primaryColor,
+                        fontWeight: FontWeight.w400,
+                      ),),
+                      Container(
+                        margin: EdgeInsets.only(left: 8.w),
+                        child: Image.asset("assets/contacts/call.png"),
+                      )
+                    ],
+                  )
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 60.w),
+                    child: Row(
+
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(title2, style: TextStyle(color: Color(0xff00A6D6),
+                            fontFamily: "roboto_bold",
+                            fontSize: 13.sp)),
+                      ],
+                    ),
+                  )
+
+                  , Container(
+                    margin: EdgeInsets.only(left: 60.w),
+
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Text(subTitle2, style: TextStyle(
+
+                              fontSize: 16.sp,
+                              fontFamily: "roboto_bold",
+                              color: AppColors.primaryColor,
+                              fontWeight: FontWeight.w400,
+                            ),),
+                            Container(
+                              margin: EdgeInsets.only(left: 16.w),
+                              child: Image.asset("assets/contacts/call.png"),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+
+}
+}
