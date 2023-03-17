3195cde15bbf023d6522b23808d4c8271ae82ee1
codewithjaspreet <jaspreetsodhi02@gmail.com>
2023-03-17 16:58:04 +0530

    user details

--- lib/view/menu/items/contacts/Details.dart
+++ lib/view/menu/items/contacts/Details.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:happsales_crm/utils/color.dart';
 import 'package:happsales_crm/view/menu/items/contacts/widgets/CustomAppBar.dart';
+import 'package:happsales_crm/view/menu/items/contacts/widgets/HDivider.dart';
 
 class ContactDetails extends StatelessWidget {
   const ContactDetails({Key? key}) : super(key: key);
@@ -13,100 +14,110 @@ class ContactDetails extends StatelessWidget {
       appBar: CustomAppBar(context),
 
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
 
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 25.w,vertical: 15.h),
-
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.only(top: 20.h , left: 20.w),
-                      child: Row(
-                        children: [
-                          Container(
-
-                            width: 63.w,
-                            height: 63.h,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20.r),
-                              color: Colors.white,
-
-                            ),
-
-                            child: Container(
-                              margin: EdgeInsets.only(left: 2.w),
-                              child: Center(
-                                child: Text("ST",style: TextStyle(color: AppColors.primaryColor,fontSize: 44.sp),),
-                              ),
-                            ),
-                          ),
-                          Container(
-                            margin: EdgeInsets.only(left: 20.w),
-                            child:  Text("Suvarna traders",style: TextStyle(color: Colors.white,fontSize: 20.sp),),
-                          ),
-
-                        ],
-                      ),
-
-                    ),
-
-                    Container(
-                      margin: EdgeInsets.only(top: 15.h , left: 25.w),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                            child: Text("Radha Krishna",style: TextStyle(color: Colors.white,fontSize: 20.sp,fontFamily: "roboto_bold"),),
-                          ),
-                          Container(
-                            child: Text("Maintenance Manager",style: TextStyle(color: Colors.white,fontSize: 15.sp),),
-                          ),
-                        ]
-                      ),
-                    ),
-
-                   Container(
-                      margin: EdgeInsets.only(top: 15.h , left: 25.w),
-                     child: Column(
-                       crossAxisAlignment: CrossAxisAlignment.start,
-                       children: [
-                         Container(
-                           child: Text("99999 12345",style: TextStyle(color: Colors.white,fontSize: 14.sp ,fontFamily: "roboto_bold"),),
-                         ),
-                         Container(
-                           child: Text("radha.krishna@SuvarnaTraders.com",style: TextStyle(color: Colors.white,fontSize: 14.sp ,fontFamily: "roboto_bold"),),
-                         ),
-                       ],
-                     ),
-                   )
-                  ],
-                ),
-                width: 337.w,
-                height: 216.h,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10.r),
-                  gradient: LinearGradient(
-                    begin: Alignment.topLeft,
-                    end: Alignment.bottomRight,
-                    colors: [
-                      Color(0xffAA80F9),
-                      Color(0xff6165D7),
-                    ],
-                  )
+          UserDetailCard(),
+          Hdivider(),
+          DetailRow(),
 
 
 
-                ),
-              ),
-            ],
-          )
         ],
       ),
     );
+  }
+
+  Column UserDetailCard() {
+    return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 25.w,vertical: 15.h),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 20.h , left: 20.w),
+                    child: Row(
+                      children: [
+                        Container(
+
+                          width: 63.w,
+                          height: 63.h,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20.r),
+                            color: Colors.white,
+
+                          ),
+
+                          child: Container(
+                            margin: EdgeInsets.only(left: 2.w),
+                            child: Center(
+                              child: Text("ST",style: TextStyle(color: AppColors.primaryColor,fontSize: 44.sp),),
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(left: 20.w),
+                          child:  Text("Suvarna traders",style: TextStyle(color: Colors.white,fontSize: 20.sp),),
+                        ),
+
+                      ],
+                    ),
+
+                  ),
+
+                  Container(
+                    margin: EdgeInsets.only(top: 15.h , left: 25.w),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          child: Text("Radha Krishna",style: TextStyle(color: Colors.white,fontSize: 20.sp,fontFamily: "roboto_bold"),),
+                        ),
+                        Container(
+                          child: Text("Maintenance Manager",style: TextStyle(color: Colors.white,fontSize: 15.sp),),
+                        ),
+                      ]
+                    ),
+                  ),
+
+                 Container(
+                    margin: EdgeInsets.only(top: 15.h , left: 25.w),
+                   child: Column(
+                     crossAxisAlignment: CrossAxisAlignment.start,
+                     children: [
+                       Container(
+                         child: Text("99999 12345",style: TextStyle(color: Colors.white,fontSize: 14.sp ,fontFamily: "roboto_bold"),),
+                       ),
+                       Container(
+                         child: Text("radha.krishna@SuvarnaTraders.com",style: TextStyle(color: Colors.white,fontSize: 14.sp ,fontFamily: "roboto_bold"),),
+                       ),
+                     ],
+                   ),
+                 )
+                ],
+              ),
+              width: 337.w,
+              height: 216.h,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10.r),
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color(0xffAA80F9),
+                    Color(0xff6165D7),
+                  ],
+                )
+
+
+
+              ),
+            ),
+          ],
+        );
   }
 }
