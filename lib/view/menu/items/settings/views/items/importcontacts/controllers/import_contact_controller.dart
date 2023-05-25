02dbb7b0ab33b76894c9aeeb4eb9a40bf33b5708
jaspreetsinghsodhi <jaspreetsodhi02@gmail.com>
2023-05-25 12:45:29 +0530

    importing contactS

--- lib/view/menu/items/settings/views/items/importcontacts/controllers/import_contact_controller.dart
+++ lib/view/menu/items/settings/views/items/importcontacts/controllers/import_contact_controller.dart
@@ -1,9 +1,58 @@
-import 'dart:html';
 
+import 'package:contacts_service/contacts_service.dart';
 import 'package:get/get.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 class ImportContactController extends GetxController{
 
+  @override
   
+  void onInit() {
+    super.onInit();
+    permissionCheck();
+    fetchContacts();
+
+    
+  }
+
+  List<Contact> contactList = [];
+  List<Contact> selectedContactList = [];
+  RxBool isLoading= true.obs;
+  RxBool isCheckboxSelected = false.obs;
+
+  void permissionCheck() async {
+
+    if(await Permission.contacts.isGranted){
+
+      fetchContacts();
+    }
+    
+
+    else{
+      Permission.contacts.request();
+    }
+  }
+
+
+  void fetchContacts() async {
+    
+    contactList  = await ContactsService.getContacts();
+    isLoading.value = false;
+    // update();
+
+
+ }
+
+ void Mapping(){
+
+  for(int i = 0 ; i < contactList.length ; i++){
+
+    if(isCheckboxSelected.value == true){
+      selectedContactList.add(contactList[i]);
+    }
+  }
+  update();
+ }
+
 }
+
--- lib/view/menu/items/settings/views/items/importcontacts/views/import_contact.dart
+++ lib/view/menu/items/settings/views/items/importcontacts/views/import_contact.dart
@@ -1,10 +1,14 @@
+import 'dart:math' as math;
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:get/get_state_manager/src/rx_flutter/rx_obx_widget.dart';
 import 'package:happsales_crm/view/menu/items/contacts/widgets/CustomAppBar.dart';
 import 'package:happsales_crm/view/menu/items/contacts/widgets/HelperRow.dart';
-
+import 'package:happsales_crm/view/menu/items/settings/views/items/importcontacts/controllers/import_contact_controller.dart';
 import '../../../../../../../../utils/color.dart';
 import '../../../../../../../voiceassistant/sheet.dart';
 
@@ -68,11 +72,121 @@ class ImportContact extends StatelessWidget {
         ],
       ),
     ),
+    _buildEmployeeListView()
 
         ],),
 
-        bottomDetailsSheet()
+        // bottomDetailsSheet()
       ]),
     );
   }
+}
+
+_buildEmployeeListView() {
+
+  ImportContactController importContactController = Get.put(ImportContactController());
+ 
+ 
+  return Obx(() =>  Expanded(
+      child: importContactController.isLoading.value ? const CircularProgressIndicator(): ListView.builder(
+        padding: EdgeInsets.all(12.sp),
+        itemCount: importContactController.contactList.length,
+        // itemCount: contactController.contacts.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: (){
+
+              
+             },
+            child: Container(
+              margin: EdgeInsets.only(top : 12.sp),
+              height: 90.h,
+                width: 337.w,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.grey.withOpacity(0.4)),
+
+                
+                borderRadius: BorderRadius.circular(10.r),
+
+              ),
+
+              child: Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left : 12.sp),
+                    width: 50.w,
+                    height: 50.w,
+                    decoration:  BoxDecoration(
+                      color: Color((math.Random().nextDouble() * 0xFFFFFF).toInt()).withOpacity(1.0)
+,
+                      shape: BoxShape.circle
+                    ),
+                    child: GestureDetector(
+                      onTap: (){
+                        // Get.to(const AddContact());
+                      },
+                      child: Center(
+                        child: Text(importContactController.contactList[index].givenName!.substring(0,1).toUpperCase() .toUpperCase(),style: TextStyle(
+                          color: Colors.white,
+                          fontFamily: 'roboto_bold',
+                          fontSize: 20.sp
+                        ),),
+                        
+                        
+                      ),
+                    ),
+                  ),
+
+                  Container(
+                    width: 150.w,
+                    child: Column(
+                    
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                    
+                          margin: EdgeInsets.only(left : 16.sp),
+                          child:Text(importContactController.contactList[index].givenName!,style: TextStyle(
+                            color: AppColors.primaryColor,
+                            fontFamily: 'roboto_bold',
+                            fontSize: 16.sp
+                          ),),
+                      
+                        ),Container(
+                          margin: EdgeInsets.only(left : 16.sp,top: 2.sp,),
+                          child:Text(importContactController.contactList[index].phones![0].value!,style: TextStyle(
+                              color: AppColors.primaryColor,
+                              fontFamily: 'roboto_regular',
+                              fontSize: 14.sp
+                          
+                          ),),
+                        ),
+                      ],
+                    ),
+                  ),
+
+              Obx(() => 
+                   Container(
+                      margin: EdgeInsets.only(left : 80.sp),
+                     child: GestureDetector(
+                                   onTap: () {
+                                    
+                                     
+                                   },
+                                   child: Icon(
+                     importContactController.isCheckboxSelected.value? Icons.check_box : Icons.check_box_outline_blank,
+                                     color: Colors.grey.withOpacity(0.3) ,
+                                   ),
+                                 ),
+                   ),
+                  
+)
+                  
+                ],
+              ),
+            ),
+          );
+        },
+      )));
 }
